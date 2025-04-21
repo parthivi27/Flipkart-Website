6a8cdfa42f889be8af735c1fb08ac69579d08cd1
parthivi27 <124782087+parthivi27@users.noreply.github.com>
2025-04-21 15:41:58 +0530

    Delete Amazon directory

--- Amazon/electronics/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Product
-
-class ProductForm(forms.ModelForm):
-    model = Product
-    class Meta:
-        model = Product
-        fields = "__all__"
--- Amazon/electronics/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from .models import Product
-from .forms import ProductForm
-
-# Create your views here.
-def home(request):
-    prod=Product.objects.all()
-    fm = ProductForm()
-    return render(request,'Electronics/home.html',{'prod':prod,'fm':fm})
